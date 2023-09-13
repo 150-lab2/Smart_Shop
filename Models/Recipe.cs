a4a6a5db4f08c119a14d70636469640eeb02bd5c
RyanMcArthur <merccat@mail.fresnostate.edu>
2023-09-12 19:26:04 -0700

    Add project files.

---
+++ Models/Recipe.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class Recipe
+    {
+        public Guid Id { get; init; }
+        public string Name { get; init; }
+        public ICollection<GroceryItem> Items { get; init; }
+
+        public static Recipe Create(string name, GroceryItem[] items)
+        {
+            return new Recipe()
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Items = items
+            };
+        }
+    }
+}
---
+++ WebAPI/Controllers/GroceryController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace WebAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GroceryController : ControllerBase
+    {
+        private readonly ILogger<GroceryController> _logger;
+
+        public GroceryController(ILogger<GroceryController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet(Name = "GetGroceryList")]
+        public GroceryItem[] List()
+        {
+            return new GroceryItem[]
+            {
+                GroceryItem.Create( "Beans", "WalMart", "Canned Goods", 1.99m),
+                GroceryItem.Create( "Tortillas", "WalMart", "Bakery", 2.50m ),
+                GroceryItem.Create( "Cheese", "WalMart", "Deli", 3.75m ),
+            };
+        }
+    }
+
+
+}
