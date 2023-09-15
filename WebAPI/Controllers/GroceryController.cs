77e0d256c3e632d4f101e23b65ebd27adbc0a73b
RyanMcArthur <merccat@mail.fresnostate.edu>
2023-09-14 18:55:08 -0700

    Setup a couple endpoints that use our local database

--- WebAPI/Controllers/GroceryController.cs
+++ WebAPI/Controllers/GroceryController.cs
@@ -1,29 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Data;
+using Microsoft.AspNetCore.Mvc;
 using Models;
 
 namespace WebAPI.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("[controller]/[action]")]
     public class GroceryController : ControllerBase
     {
         private readonly ILogger<GroceryController> _logger;
+        private readonly SmartShopContext _dbContext;
 
-        public GroceryController(ILogger<GroceryController> logger)
+        public GroceryController(ILogger<GroceryController> logger, SmartShopContext dbContext)
         {
             _logger = logger;
+            _dbContext = dbContext;
         }
 
 
         [HttpGet(Name = "GetGroceryList")]
         public GroceryItem[] List()
         {
-            return new GroceryItem[]
-            {
-                GroceryItem.Create( "Beans", "WalMart", "Canned Goods", 1.99m),
-                GroceryItem.Create( "Tortillas", "WalMart", "Bakery", 2.50m ),
-                GroceryItem.Create( "Cheese", "WalMart", "Deli", 3.75m ),
-            };
+            var groceryItems = _dbContext.Set<GroceryItem>()
+                .ToArray();
+            return groceryItems;
+        }
+
+
+        [HttpGet(Name = "GetGroceryItem")]
+        public ActionResult<GroceryItem> Item(Guid id)
+        {
+            var item = _dbContext.Set<GroceryItem>()
+                .SingleOrDefault(x => x.Id == id);
+
+            if(item == null) { return NotFound(); }
+
+            return item;
+        }
+
+
+
+        [HttpPost(Name = "AddItem")]
+        public StatusCodeResult Add(GroceryItem item)
+        {
+            if (item == null) { return BadRequest(); }
+
+            _dbContext.Add(item);
+            _dbContext.SaveChanges();
+            return Ok();
         }
     }
 
