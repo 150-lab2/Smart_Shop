186e38b756d053f30ddaf9275a635828bb20254d
RyanMcArthur <merccat@mail.fresnostate.edu>
2023-09-21 19:31:47 -0700

    Setup WebUI And Unit Tests

---
+++ SmartShop.UI/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SmartShop.UI.Models;
+using System.Diagnostics;
+
+namespace SmartShop.UI.Controllers
+{
+    public class Recpie
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var model = new List<Recpie>()
+            {
+                new Recpie()
+                {
+                    Name = "Test1",
+                    Description = "Really goood meal"
+                },
+                new Recpie ()
+                {
+                    Name = "Test 2",
+                    Description = "Kinda Gross"
+                },
+                new Recpie()
+                {
+                    Name = "Test 3",
+                    Description = "Pick Me!"
+                }
+            };
+
+            return View(model);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
