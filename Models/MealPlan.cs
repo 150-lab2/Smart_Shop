d66b4435ff6a8700a44a99c333786089d50e53c2
RyanMcArthur <merccat@mail.fresnostate.edu>
2023-10-03 18:34:23 -0700

    Working on model changes.

---
+++ Models/MealPlan.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+    public class MealPlan
+    {
+        public Guid UserId { get; set; }
+        public DateTime GeneratedDate { get; set; }
+
+        public List<GroceryItem> ShoppingList = new List<GroceryItem>();
+
+        public void CalculateShoppingList()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
