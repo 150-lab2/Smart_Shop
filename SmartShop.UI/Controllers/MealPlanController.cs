25babff0b87a6cb82062fdc8bb4a52c04e80bea8
RyanMcArthur <merccat@mail.fresnostate.edu>
2023-11-14 19:44:52 -0800

    UI Update Show a Recpie

---
+++ SmartShop.UI/Controllers/MealPlanController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SmartShop.UI.Controllers
+{
+    public class MealPlanController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public MealPlanController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IActionResult Index()
+        {
+            // we want to access the back end api
+
+            var client = _httpClientFactory.CreateClient("SmartShopClient");
+
+            return View();
+        }
+    }
+}
---
+++ SmartShop.UI/Controllers/RecipeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace SmartShop.UI.Controllers
+{
+    public class RecipeController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RecipeController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Suggestion()
+        {
+            // Retrieve our local user, if not existing, create a new user profile
+            var smartShopClient = _httpClientFactory.CreateClient("SmartShopClient");
+
+            var rawJsonData = await smartShopClient.GetStringAsync($"/api/Recipe/Random?count=5");
+
+
+            return View("Recipe", rawJsonData);
+        }
+    }
+}
