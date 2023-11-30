a13f632954f7dac2396b4dfa591f4f2d03a506ad
zache <zache@LAPTOP-NQN18MDC>
2023-11-29 16:49:41 -0800

    Changes to controllers, layout, and userprofile. Created index for recipe page, which allows view of recipe catalogue.

--- SmartShop.UI/Controllers/MealPlanController.cs
+++ SmartShop.UI/Controllers/MealPlanController.cs
@@ -11,17 +11,6 @@ namespace SmartShop.UI.Controllers
             _httpClientFactory = httpClientFactory;
         }
 
-        public async Task<IActionResult> MealPlan()
-        {
-            // Retrieve our local user, if not existing, create a new user profile
-            var smartShopClient = _httpClientFactory.CreateClient("SmartShopClient");
-
-            var rawJsonData = await smartShopClient.GetStringAsync($"/api/Recipe/Random?count=5");
-
-
-            return View("MealPLan", rawJsonData);
-        }
-
         public IActionResult Index()
         {
             // we want to access the back end api
